8c77c11d711c16879770e63cc181ea6c27bc23d7
yabbby <ye.aba.net@gmail>
2025-02-17 02:31:05 +0300

    finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'catamaran': ['Catamaran', 'sans-serif'],
+      },
+      opacity: {
+        '85': '0.85',
+      },
+    },
   },
+  variants: {},
   plugins: [],
 }
