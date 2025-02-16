35435f5b1e13a18022f4825880c4495c62352076
yabbby <ye.aba.net@gmail>
2025-02-16 22:07:42 +0300

    inital commit

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Hello-World</h1>
+     Hello-World
+    </>
+  )
+}
+
+export default App
